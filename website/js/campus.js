6f2dd113a479238bbdf6463fa0a37586b8f50fd0
segun.konibire <segun.konibire@valtech.co.uk>
2017-03-08 18:35:53 +0000

    Adds in Type.js animation for homepage

--- website/js/campus.js
+++ website/js/campus.js
@@ -34,9 +34,48 @@
                 campus.properties.isMobile = true;
             }
 
-            // displays tool tips
+            // displays tooltips
             if(campus.environment.$tooltips.length > 0){
                 campus.environment.$tooltips.tooltip();
+            }
+
+            if($('#animated-text')){
+                $("#animated-text").typed({
+                    //strings: ["First sentence.", "Second sentence."],
+                    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+                    //stringsElement: null,
+                    stringsElement: $('#text-list'),
+                    // typing speed
+                    typeSpeed: 0,
+                    // time before typing starts
+                    startDelay: 0,
+                    // backspacing speed
+                    backSpeed: 0,
+                    // shuffle the strings
+                    shuffle: false,
+                    // time before backspacing
+                    backDelay: 5000,
+                    // loop
+                    loop: false,
+                    // false = infinite
+                    loopCount: 2,
+                    // show cursor
+                    showCursor: false,
+                    // character for cursor
+                    cursorChar: "|",
+                    // attribute to type (null == text)
+                    attr: null,
+                    // either html or text
+                    contentType: 'html',
+                    // call when done callback function
+                    callback: function() {},
+                    // starting callback function before each string
+                    preStringTyped: function() {},
+                    //callback for every typed string
+                    onStringTyped: function() {},
+                    // callback for reset
+                    resetCallback: function() {}
+                });
             }
 
             // toggles display of divs
